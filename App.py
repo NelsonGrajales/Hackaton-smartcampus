f9122410501a237e9e68e3d0bc7b409f20ba6705
MiguelPerfecto <145710137+MiguelPerfecto@users.noreply.github.com>
2025-09-20 17:26:01 -0500

    ActualizacionApp.py

--- App.py
+++ App.py
@@ -6,6 +6,11 @@ app = FastAPI()
 UPLOAD_DIR = './uploads'
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
+CLIENT = InferenceHTTPClient(
+    api_url="https://serverless.roboflow.com",
+    api_key="tAGWVoOgMbywsoht0OBG"
+)
+
 @app.post('/upload-image')
 async def upload_image(file: UploadFile = File(...)):
     file_path = os.path.join(UPLOAD_DIR,file.filename)
@@ -13,8 +18,14 @@ async def upload_image(file: UploadFile = File(...)):
     with open(file_path, 'wb') as f:
         f.write(content)
 
+    result = CLIENT.infer(file_path, model_id="recyclable-items/3")
+    predictions = result.get("predictions", [])
+
+    clases_detectadas = [pred["class"] for pred in predictions]
+
     return JSONResponse(content={
-        'status':'ok',
+        'status': 'ok',
         'filename': file.filename,
-        'message': "Imagen recibida y guardada"
+        'message': "Imagen recibida y clasificada",
+        'clases_detectadas': clases_detectadas
     })
